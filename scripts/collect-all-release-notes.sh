ab62e2126d472ffe4ac2f16988282aef1345c6e2
Sebastiaan van Steenis <superseb@users.noreply.github.com>
2024-07-02 14:22:03 +0200

    Update collect-all-release-notes.sh

--- scripts/collect-all-release-notes.sh
+++ scripts/collect-all-release-notes.sh
@@ -16,13 +16,13 @@ function ghretry()
 
 > release-notes/README.md
 echo "# Qdrant" >> release-notes/README.md
-for minor in v1.9 v1.8 v1.7 v1.6 v1.5 v1.4 v1.3 v1.2 v1.1 v1.0; do
+for minor in v1.10 v1.9 v1.8 v1.7 v1.6 v1.5 v1.4 v1.3 v1.2 v1.1 v1.0; do
     product=qdrant
     > release-notes/${product}-${minor}.md
     qdrantversiontmp=$(mktemp)
     echo "| Version | Date | US date | EU date |" >> $qdrantversiontmp
     echo "| ------- | ---- | ------- | ------- |" >> $qdrantversiontmp
-    for patch in $(ghretry release list -R "qdrant/${product}" --exclude-drafts --exclude-pre-releases --limit=1000 | awk -F '\t' '{ print $3 }' | grep ^"${minor}"); do
+    for patch in $(ghretry release list -R "qdrant/${product}" --exclude-drafts --exclude-pre-releases --limit=1000 | awk -F '\t' '{ print $3 }' | grep ^"${minor}\."); do
         publish_date=$(ghretry release view "${patch}" -R "qdrant/${product}" --json publishedAt -q '.publishedAt' | awk -F'T' '{ print $1 }')
         echo "| [${patch}](${product}-${minor}.md#release-$(generate_markdown_link $patch)) | $(date +"%b %d %Y" -d "${publish_date}") | $(date +"%D" -d "${publish_date}") | $(date +"%F" -d "${publish_date}") |" >> $qdrantversiontmp
         echo "# Release ${patch}" >> release-notes/${product}-${minor}.md
